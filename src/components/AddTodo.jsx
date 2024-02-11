e0242599a7ce672c923486d1c48213f7a6387e85
Omkarnbh87 <contact.omkar87@gmail.com>
2024-02-11 21:48:43 +0530

    First Commit

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,24 @@
+function AddTodo() {
+  return (
+    <div className="container">
+      <div className="row ok-row">
+        <div className="col-6">
+          {" "}
+          <input type="text" placeholder="Enter Todo Here" />
+        </div>
+        <div className="col-4">
+          {" "}
+          <input type="date" />
+        </div>
+        <div className="col-2">
+          {" "}
+          <button type="button" className="btn btn-success ok-button">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
